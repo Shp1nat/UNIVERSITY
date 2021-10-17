1eb40da564627b29e9d30db668e82533025034c5
Shp1nat <stac987653210@mail.ru>
2021-10-17 21:15:59 +0300

    Задания к теме 10

---
+++ Задания к теме 10/1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b;
+	cout << "Введите целое число A: ";
+	cin >> a;
+	cout << "Введите целое число A: ";
+	cin >> b;
+	if ((a > 2) && (b <= 3))
+	{
+		cout << "Высказывание 'Справедливы неравенства A > 2 и B <= 3' является истиной";
+	}
+	else
+	{
+		cout << "Высказывание 'Справедливы неравенства A > 2 и B <= 3' является ложью";
+	}
+	return 0;
+}
---
+++ Задания к теме 10/2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cout << "Введите целое число A: ";
+	cin >> a;
+	cout << "Введите целое число B: ";
+	cin >> b;
+	cout << "Введите целое число C: ";
+	cin >> c;
+	if ((a < b) && (b < c))
+	{
+		cout << "Высказывание 'Справедливо двойное неравенство A < B < C' является истиной";
+	}
+	else
+	{
+		cout << "Высказывание 'Справедливо двойное неравенство A < B < C' является ложью";
+	}
+	return 0;
+}
