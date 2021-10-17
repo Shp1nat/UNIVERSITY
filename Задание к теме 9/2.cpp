fc21f377da9e7aefb4361da4476133d72106079f
Shp1nat <stac987653210@mail.ru>
2021-10-17 18:54:09 +0300

    Задания к теме 9

---
+++ Задание к теме 9/2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cout << "Введите целое число, лежащее в диапазоне 1–365: ";
+	cin >> a;
+	if ((a < 1) || (a>365))
+	{
+
+		cout << "Число не удовлетворяет диапазону 1-365";
+	}
+	else
+	{
+		a = a % 7;
+		cout << "Номер дня недели для этого дня года: " << a;
+	}
+	return 0;
+}
---
+++ Задание к теме 9/5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a;
+	cout << "Введите год: ";
+	cin >> a;
+	if (a < 1) 
+	{
+
+		cout << "Год должен быть целым положительным числом";
+	}
+	else
+	{
+		a = (a - 1) / 100 + 1;
+		cout << "Этот год является " << a << " столетием";
+	}
+	return 0;
+}
